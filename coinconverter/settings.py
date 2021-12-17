38a7b9b08864f8d461f87e72ad5e64fb0bfe52b5
Gustavo Gonçalves <gustavo.pirro@gmail.com>
2021-12-17 14:42:53 -0300

    config: added secret key .env file and python-dotenv dependency

--- coinconverter/settings.py
+++ coinconverter/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sf*!i^%yfk4!0pvx1=eda0uywvxx4&3*v69u2%_d746ir8&ga8'
+load_dotenv(find_dotenv())
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,8 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'converter_api'
+    #REST API
+    'rest_framework',
+    'converter_api',
 ]
 
 MIDDLEWARE = [

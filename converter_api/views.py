66abe3b5758720b284959533d30856e438f3a2be
Gustavo Gonçalves <gustavo.pirro@gmail.com>
2021-12-17 22:06:02 -0300

    feat: Implemented Currency Class
    Class used to create a currency instance and convert it to other currencies through it's methods

--- converter_api/views.py
+++ converter_api/views.py
@@ -1,6 +1,8 @@
-from django.http.response import HttpResponse, JsonResponse
+from django.http.response import JsonResponse
 from rest_framework.views import APIView
-
+from decimal import *
+import requests
+import json
 
 class CoinConverter(APIView):
     """
@@ -23,4 +25,40 @@ class CoinConverter(APIView):
             return JsonResponse(status=404, data={'status': 'false', 'message': 'Invalid url params'})
 
         return JsonResponse({'from_coin': from_coin, 'to_coin': to_coin, 'coin_amount': coin_amount})
-    
+
+class Currency():
+    """
+    Class implements a currency object and
+    implements methods to convert it to other currencies
+
+    :params
+            -?currency_code - three letters code that represents the currency name,
+             possible choices can be found at the URL below
+             https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json
+    """
+
+    def __init__(self, currency_code):
+        self.currency_code = currency_code.lower()
+
+    def convert_to_usd(self, amount):
+        url= f'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/{self.currency_code}.json'
+        response = requests.get(url)
+        dict_response = json.loads(response.text)
+        currency_in_usd = Decimal(str(dict_response[self.currency_code]['usd']))
+
+        return currency_in_usd * Decimal(str(amount))
+
+    def convert_usd_to_currency(self, final_currency):
+        cleaned_data_currency = final_currency.lower()
+        url= f'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json'
+        response = requests.get(url)
+        dict_response = json.loads(response.text)
+        usd_to_currency = Decimal(str(dict_response['usd'][cleaned_data_currency]))
+
+        return usd_to_currency
+
+    def convert_to_currency(self, to_currency, amount):
+        currency_in_usd = self.convert_to_usd(amount)
+        usd_to_currency = self.convert_usd_to_currency(to_currency)
+
+        return currency_in_usd * usd_to_currency
